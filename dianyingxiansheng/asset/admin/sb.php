e11f2957fe59ea1257da16da4a9134b3a59a9f08
brevis-ng <brevisnguyen@gmail.com>
2022-03-12 18:04:52 +0800

    First commit

---
+++ dianyingxiansheng/asset/admin/sb.php
@@ -0,0 +1,30 @@
+<?php
+return array (
+  'dscms' => 
+  array (
+    'jc' => 
+    array (
+      'logo1' => 'template/dianyingim/img/index-logo.png',
+      'logo2' => 'template/dianyingim/img/logo2.png',
+      'icon' => 'template/dianyingim/img/logo.png',
+      'pic' => 'template/dianyingim/img/loading.png',
+      'gg' => '感谢使用电影先生开源项目',
+      'txt' => '<p>电影先生</p>
+<p>模板交流qq群：872882160</p>
+<p>如遇到任何问题可加作者群反馈</p>
+<p>作者qq群：<a href="https://jq.qq.com/?_wv=1027&k=ctsUKe9L" target="_blank"><strong>点击这里快速加入群聊</strong></a>',
+      'zhizhang' => 'Cảm ơn bạn đã sử dụng mã nguồn này, nếu bạn thích nó, hãy truyền bá nó 😊',
+      'sm' => 'Tất cả nội dung trên trang web này đến từ các tài nguyên được tham chiếu công khai do các trang web chia sẻ Internet cung cấp và không cung cấp dịch vụ tải lên và lưu trữ tài nguyên video.',
+    ),
+    'nav' => 
+    array (
+      'actor' => '1',
+      'topic' => '1',
+      'vip' => '1',
+      'ivi' => '1',
+      'zdyname1' => 'aaa1',
+      'zdyurl1' => '111',
+    ),
+    'indexactor' => '1',
+  ),
+);
